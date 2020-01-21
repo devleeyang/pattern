854dc69043f5b64f75b86667d8aa3e37f288e7b8
양혜리 <leeyang6424@interpark.com>
2020-01-21 17:00:42 +0900

    MVVM패턴 적용중...

---
+++ Pattern/Pattern/MVVM/View/MVVMLoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MVVMLoginViewController.swift
+//  Pattern
+//
+//  Created by 양혜리 on 21/01/2020.
+//  Copyright © 2020 양혜리. All rights reserved.
+//
+
+import UIKit
+import RXSwi
+
+class MVVMLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
